35bf68a79148815901c5296956833e0dd413de13
giomio <50331971+giomio@users.noreply.github.com>
2019-05-06 17:39:04 +0200

    update
    descriptions

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
-def get_data (numberofnumbers):
+def get_data (numberofnumbers): #this function recives the size of list and then let user to add numbers as its elements
     list=[]
     for i in range (numberofnumbers):
         add = input()
         list.append(add)
     return list
-print("How many elements may your list have?")
+print("How many elements may your list have?") #asking user to insert list size
 
 amount=int(input())
 
 
-print(get_data(amount))
+print(get_data(amount)) #requesting created function
